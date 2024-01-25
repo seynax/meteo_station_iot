088c762c5d6f445ae947371208b513a923656a51
achraf768 <achraf.atmani@viacesi.fr>
2024-01-25 22:16:19 +0100

    ajouts de commentaires

--- api/api.py
+++ api/api.py
@@ -1,12 +1,15 @@
-import flask
-from flask_cors import CORS
-import sqlite3
+# Importation des modules nécessaires
+import flask  # Flask pour créer un serveur web.
+from flask_cors import CORS  # Gestion des requêtes cross-origin (CORS).
+import sqlite3  # Interaction avec la base de données SQLite.
 
+# Création de l'application Flask
 app = flask.Flask(__name__, template_folder='views')
 CORS(app)
 
-# fonction emoji selon la temperature
+# Fonction pour déterminer l'emoji en fonction de la température
 def determiner_pictogramme(temperature):
+    # Si la température est égale ou supérieure à 20, renvoie un emoji soleil, sinon un emoji neige.
     if temperature >= 20:
         return '☀️'
     else:
@@ -15,7 +18,7 @@ def determiner_pictogramme(temperature):
 # Route pour créer un compte utilisateur
 @app.route('/api/signup', methods=['POST'])
 def signup():
-    data = flask.request.get_json()
+    data = flask.request.get_json()  # Récupère les données JSON de la requête.
     pseudo = data.get('pseudo')
     mot_de_passe = data.get('mot_de_passe')
     confirmation_mot_de_passe = data.get('confirmation_mot_de_passe')
@@ -43,7 +46,7 @@ def signup():
 # Route pour ajouter un relevé
 @app.route('/api/releves', methods=['POST'])
 def ajouter_releve():
-    data = flask.request.get_json()
+    data = flask.request.get_json()  # Récupère les données JSON de la requête.
     humidite = data.get('humidite')
     temperature = data.get('temperature')
     pression = data.get('pression')
@@ -52,22 +55,29 @@ def ajouter_releve():
 
     connection = sqlite3.connect("baseDeDonnee.db")
     cursor = connection.cursor()
+
+    # Insertion du relevé dans la base de données
     cursor.execute('INSERT INTO releves (id_sonde, temperature, humidite, pression, date_releve) VALUES (?,?,?,?,?)',
                    (id_sonde, temperature, humidite, pression, date_releve))
     connection.commit()
     connection.close()
 
     return flask.jsonify({"message": "Relevé bien ajouté"})
+
+# Route pour récupérer tous les relevés
 @app.route('/api/releves/', methods=['GET'])
 def recuperer_releves():
     conn = sqlite3.connect("baseDeDonnee.db")
     cursor = conn.cursor()
+
+    # Sélectionner tous les relevés de la base de données
     cursor.execute('SELECT * FROM releves')
     releves = cursor.fetchall()
     conn.close()
 
     liste_releves = []
 
+    # Transformation des relevés en format JSON
     for releve in releves:
         dico = {'id_releve': releve[0],
                 'id_sonde': releve[1],
@@ -79,16 +89,20 @@ def recuperer_releves():
 
     return flask.jsonify(liste_releves)
 
+# Route pour récupérer les relevés d'une sonde spécifique
 @app.route('/api/releves/<int:id_sonde>', methods=['GET'])
 def recuperer_releves_sonde(id_sonde):
     conn = sqlite3.connect("baseDeDonnee.db")
     cursor = conn.cursor()
+
+    # Sélectionner les relevés spécifiques d'une sonde dans la base de données
     cursor.execute('SELECT * FROM releves WHERE id_sonde = ?', (id_sonde,))
     releves = cursor.fetchall()
     conn.close()
 
     liste_releves = []
 
+    # Transformation des relevés en format JSON
     for releve in releves:
         dico = {'id_releve': releve[0],
                 'id_sonde': releve[1],
@@ -100,78 +114,89 @@ def recuperer_releves_sonde(id_sonde):
 
     return flask.jsonify(liste_releves)
 
+# Route pour ajouter une sonde
 @app.route('/api/ajouter-sonde', methods=['POST'])
 def ajouter_sonde():
-    data = flask.request.get_json()
+    data = flask.request.get_json()  # Récupère les données JSON de la requête.
     nom = data.get('nom')
 
     conn = sqlite3.connect("baseDeDonnee.db")
     cursor = conn.cursor()
+
+    # Insertion de la sonde dans la base de données
     cursor.execute('INSERT INTO sondes (nom) VALUES (?)', (nom,))
     conn.commit()
     conn.close()
 
     return flask.jsonify({"message": "Sonde bien ajoutée"})
 
+# Route pour supprimer une sonde
 @app.route('/api/supprimer-sonde', methods=['POST'])
 def supprimer_sonde():
-    data = flask.request.get_json()
+    data = flask.request.get_json()  # Récupère les données JSON de la requête.
     id_sonde = data.get('id_sonde')
 
     conn = sqlite3.connect("baseDeDonnee.db")
     cursor = conn.cursor()
+
+    # Suppression de la sonde de la base de données
     cursor.execute('DELETE FROM sondes WHERE id_sonde = ?', (id_sonde,))
     conn.commit()
     conn.close()
 
     return flask.jsonify({"message": "Sonde bien supprimée"})
 
+# Route pour activer une sonde
 @app.route('/api/activer-sonde', methods=['POST'])
 def activer_sonde():
-    data = flask.request.get_json()
+    data = flask.request.get_json()  # Récupère les données JSON de la requête.
     id_sonde = data.get('id_sonde')
 
     conn = sqlite3.connect("baseDeDonnee.db")
     cursor = conn.cursor()
+
+    # Activation de la sonde dans la base de données
     cursor.execute('UPDATE sondes SET etat = 1 WHERE id_sonde = ?', (id_sonde,))
     conn.commit()
     conn.close()
 
     return flask.jsonify({"message": "Sonde bien activée"})
 
+# Route pour désactiver une sonde
 @app.route('/api/desactiver-sonde', methods=['POST'])
 def desactiver_sonde():
-    data = flask.request.get_json()
+    data = flask.request.get_json()  # Récupère les données JSON de la requête.
     id_sonde = data.get('id_sonde')
 
     conn = sqlite3.connect("baseDeDonnee.db")
     cursor = conn.cursor()
+
+    # Désactivation de la sonde dans la base de données
     cursor.execute('UPDATE sondes SET etat = 0 WHERE id_sonde = ?', (id_sonde,))
     conn.commit()
     conn.close()
 
+    return flask.jsonify({"message": "Sonde bien désactivée"})
 
-    # Route pour que l'admin recupere tout
+# Route pour récupérer toutes les données pour une sonde spécifique (utilisateur)
 @app.route('/api/user/recup-toutes-les-donnees', methods=['GET'])
 def recuperer_toutes_les_donnees():
     conn = sqlite3.connect("baseDeDonnee.db")
     cursor = conn.cursor()
 
-    # on recup tout les données
+    # Sélectionner tous les relevés dans la base de données
     cursor.execute('SELECT * FROM releves')
     releves = cursor.fetchall()
 
-
-  # on récupère les relevés spécifiques d'une  seule sonde
+    # Sélectionner les relevés spécifiques d'une seule sonde dans la base de données
     cursor.execute('SELECT * FROM releves WHERE id_sonde = ?', (id_sonde,))
     releves = cursor.fetchall()
-
 
     conn.close()
 
     liste_releves = []
 
-    
+    # Transformation des relevés en format JSON
     for releve in releves:
         dico = {'id_releve': releve[0],
                 'id_sonde': releve[1],
@@ -179,27 +204,25 @@ def recuperer_toutes_les_donnees():
                 'humidite': releve[3],
                 'pression': releve[4],
                 'date_releve': releve[5],
-                'pictogramme': determiner_pictogramme(releve[2])},
+                'pictogramme': determiner_pictogramme(releve[2])}
         liste_releves.append(dico)
 
-    
     return flask.jsonify(liste_releves)
 
-    return flask.jsonify({"message": "Sonde désactivée"})
-
+# Route pour récupérer toutes les données pour un administrateur
 @app.route('/api/admin/recup-toutes-les-donnees', methods=['GET'])
 def recuperer_toutes_les_donnees_admin():
     conn = sqlite3.connect("baseDeDonnee.db")
     cursor = conn.cursor()
 
-    # on recup tout les données
+    # Sélectionner tous les relevés dans la base de données
     cursor.execute('SELECT * FROM releves')
     releves = cursor.fetchall()
     conn.close()
 
     liste_releves = []
 
-    
+    # Transformation des relevés en format JSON
     for releve in releves:
         dico = {'id_releve': releve[0],
                 'id_sonde': releve[1],
@@ -207,8 +230,11 @@ def recuperer_toutes_les_donnees_admin():
                 'humidite': releve[3],
                 'pression': releve[4],
                 'date_releve': releve[5],
-                'pictogramme': determiner_pictogramme(releve[2])},
+                'pictogramme': determiner_pictogramme(releve[2])}
         liste_releves.append(dico)
 
+    return flask.jsonify(liste_releves)
+
+# Exécution de l'application Flask
 if __name__ == '__main__':
     app.run(host="192.168.124.128", debug=True)
